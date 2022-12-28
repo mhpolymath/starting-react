7117b390bcaf55c15a37efb994e85d6dd3ff5460
mhpolymath <mhpolymath@gmail.com>
2022-12-28 22:53:36 +0300

    Updates

---
+++ precache-manifest.00df6d4ce33ad893879b8da2701c8441.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a5f30a8168cec9e13d3",
+    "url": "/starting-react/static/css/main.b972310b.chunk.css"
+  },
+  {
+    "revision": "0a5f30a8168cec9e13d3",
+    "url": "/starting-react/static/js/main.0a5f30a8.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/starting-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/starting-react/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "570184095d5672fa3a2e",
+    "url": "/starting-react/static/js/runtime~main.57018409.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/starting-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9966ea170147d01871d3a32f23b93a63",
+    "url": "/starting-react/index.html"
+  }
+];
